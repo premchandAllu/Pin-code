b4c359b659e0fd65438cbcd940049f438a9aafff
premchandAllu <premchandallu1999@gmail.com>
2020-09-30 16:11:43 +0530

    Added: 6 digit pincode is validated

--- PINcode.java
+++ PINcode.java
@@ -1,10 +1,33 @@
 package com.blz.pincode;
+import java.util.*;
+import java.util.regex.*;
 
 public class PINcode 
   {
 	public static void main(String[] args)
 	  {  
-		System.out.println("Welcome to pincode validation problem");
+		
+		Scanner sc= new Scanner(System.in);
+		  
+		  //giving output to enter name 
+		  System.out.println("Enter pincode");
+		  
+		  //Taking input from user
+		  String pincode= sc.next();
+		  
+		  String pattern="^[0-9]{6}$";
+		  Pattern pattenName= Pattern.compile(pattern);
+		  Matcher m =pattenName.matcher(pincode);
+
+                sc.close();
+		  
+		  if(m.matches())
+		  { 
+			  System.out.println("Pincode is  "+pincode);
+		  }
+		  else 
+			  System.out.println("Enter correct pincode");
+
 		 	  
 	  }
 }
