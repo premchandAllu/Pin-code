d9bcea72c2c412f0e1a58c2300664d4bc7193e1a
premchandAllu <premchandallu1999@gmail.com>
2020-09-30 16:08:49 +0530

    Welcome to PIN code problem

---
+++ PINcode.java
@@ -0,0 +1,10 @@
+package com.blz.pincode;
+
+public class PINcode 
+  {
+	public static void main(String[] args)
+	  {  
+		System.out.println("Welcome to pincode validation problem");
+		 	  
+	  }
+}
